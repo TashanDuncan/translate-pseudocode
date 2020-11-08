fda9f25fffc9124e208e518901c0574d94097a09
viewtifult <tashanduncan1994@gmail.com>
2020-11-08 11:12:22 +0000

    add js pseudocode

---
+++ gpa.js
@@ -0,0 +1,19 @@
+// function calculate_gpa
+    // pass in student_grades
+    // set grade_total to 0
+    // for each grade in student_grades
+        // if grade is not a 1, 2, 3, or 4
+            // print "invalid grade" and grade
+            // exit function with "can't complete calculation" message
+        // else add grade to grade_total
+        // endif
+    // endfor
+    // set gpa to grade_total / number of grades
+    // return gpa
+// endfunction
+
+// set reggie_grades to 4, 4, 3, 4
+// print the results of calling calculate_gpa with reggie_grades
+
+// set dave_grades to 1, 2, 3, 2
+// print the results of calling calculate_gpa with dave_grades
